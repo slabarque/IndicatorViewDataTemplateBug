713cd56814274a8377644d3a5aa3e0e0d66e94e6
Stif <stiflabarque@fisheye.eu>
2025-07-16 20:18:41 +0200

    load data with command -> THIS BREAKS (the indicators are not visible)

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace IndicatorViewDataTemplateBug;
 
@@ -13,12 +14,18 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-        var today = DateTime.Today;
-        var result = Enumerable.Range(-30, 60).Select(i => DateOnly.FromDateTime(today.AddDays(i))).ToArray();
-        foreach (var date in result)
-            Dates.Add(new MyModel { Date = date, DayOfYear = date.DayOfYear });
+        Init = new Command(() =>
+        {
+            var today = DateTime.Today;
+            var result = Enumerable.Range(-30, 60).Select(i => DateOnly.FromDateTime(today.AddDays(i))).ToArray();
+            foreach (var date in result)
+                Dates.Add(new MyModel { Date = date, DayOfYear = date.DayOfYear });
+        });
         BindingContext = this;
     }
+
     public ObservableCollection<MyModel> Dates { get; } = new();
+
+    public ICommand Init {  get; private set; }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IndicatorViewDataTemplateBug
+
+This repo demonstrates a problem when using a MAUI CarouselView and IndicatorView with IndicatorViewTemplate and populating the ItemSource in a Command i.o. in the constructor.
